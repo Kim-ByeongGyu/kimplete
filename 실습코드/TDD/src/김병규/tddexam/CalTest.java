e6cb19e4e02489d11b84c6d7eb375f87f14cebf6
Kim-ByeongGyu <zenk5554785@gmail.com>
2024-03-18 16:50:03 +0900

    김병규 파일 업로드

---
+++ 실습코드/TDD/src/김병규/tddexam/CalTest.java
@@ -0,0 +1,17 @@
+package com.example.tddexam;
+
+public class CalTest {
+    public static void main(String[] args) {
+        Calulator cal = new Calulator();
+        //plus test
+//        int result = cal.plus(10,20);
+//
+//        System.out.println(result);
+//
+        System.out.println(cal.plus(87,25));
+
+        //minus test
+//        System.out.println(cal.minus(20,10));
+
+    }
+}
