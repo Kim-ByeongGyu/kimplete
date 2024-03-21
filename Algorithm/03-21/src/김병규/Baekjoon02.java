b11956c287399bc495b42016506f4d68bbac4b47
Kim-ByeongGyu <zenk5554785@gmail.com>
2024-03-21 17:57:23 +0900

    김병규 최종 업로드

--- Algorithm/03-21/src/김병규/Baekjoon02.java
+++ Algorithm/03-21/src/김병규/Baekjoon02.java
@@ -5,19 +5,34 @@ import java.util.Scanner;
 
 public class Baekjoon02 {
     // https://www.acmicpc.net/problem/2750
+
+    static void bSort (int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("배열 크기 : ");
         int n = sc.nextInt();
         int[] arr = new int[n];
+
         // 요소 입력
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             System.out.printf("요소 %d : ", i+1);
             arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
+        bSort(arr);
 
-        for(int i=0;i<n;i++){
+        for (int i = 0;i < n; i++) {
             System.out.println(arr[i]);
         }
     }
