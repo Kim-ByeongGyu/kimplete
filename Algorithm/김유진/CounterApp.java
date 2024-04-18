13bf9a79a92d42817728f76c35b8792d84b5296d
Yoong-D <as2132as@naver.com>
2024-04-18 17:50:04 +0900

    04.18_Thread Practice 수정

--- Algorithm/김유진/CounterApp.java
+++ Algorithm/김유진/CounterApp.java
@@ -6,18 +6,28 @@ class IncrementCounter implements Runnable{
     public void run() {
         for(int i = 0; i<101; i++){
             System.out.printf("Increment: {%d}\n", i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
+// 숫자 100->1 까지 감소 시키는 스레드
 class DecrementCounter implements Runnable{
     @Override
     public void run() {
         for(int i = 100; i>-1; i--){
             System.out.printf("Decrement: {%d}\n", i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
-// 숫자 100->1 까지 감소 시키는 스레드
 public class CounterApp {
     public static void main(String[] args) {
         Thread incrementThread = new Thread(new IncrementCounter());
