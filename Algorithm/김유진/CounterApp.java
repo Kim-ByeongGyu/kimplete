8ed3c5ccc406e4c2e65e8d76457bfc8e14d805f4
Yoong-D <as2132as@naver.com>
2024-04-18 16:33:04 +0900

    04.18_Thread Practice

---
+++ Algorithm/김유진/CounterApp.java
@@ -0,0 +1,29 @@
+package day13;
+
+// 숫자 1~100까지 증가시키는 스레드
+class IncrementCounter implements Runnable{
+    @Override
+    public void run() {
+        for(int i = 0; i<101; i++){
+            System.out.printf("Increment: {%d}\n", i);
+        }
+    }
+}
+class DecrementCounter implements Runnable{
+    @Override
+    public void run() {
+        for(int i = 100; i>-1; i--){
+            System.out.printf("Decrement: {%d}\n", i);
+        }
+    }
+}
+// 숫자 100->1 까지 감소 시키는 스레드
+public class CounterApp {
+    public static void main(String[] args) {
+        Thread incrementThread = new Thread(new IncrementCounter());
+        Thread decrementThread = new Thread(new DecrementCounter());
+
+        incrementThread.start();
+        decrementThread.start();
+    }
+}
